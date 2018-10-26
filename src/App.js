21dd83ff08f0b27fff8629855d1eb21c0b8ae0b7
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-26 02:33:09 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
-
+import "./resources/styles.css";
+import Header from "./components/header-footer/Header";
 class App extends Component {
   render() {
-    return <div className="App">Hello</div>;
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
   }
 }
 
---
+++ src/components/header-footer/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import MenuIcon from "@material-ui/core/Menu";
+import IconButton from "@material-ui/core/IconButton";
+
+class Header extends Component {
+  render() {
+    return <AppBar position="fixed">Header</AppBar>;
+  }
+}
+
+export default Header;
