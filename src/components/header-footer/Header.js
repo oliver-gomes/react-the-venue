464b4e2033265d5aabcb8e351c623f636b38f2c6
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-26 02:43:16 -0400

    Header implement

--- src/components/header-footer/Header.js
+++ src/components/header-footer/Header.js
@@ -7,7 +7,23 @@ import IconButton from "@material-ui/core/IconButton";
 
 class Header extends Component {
   render() {
-    return <AppBar position="fixed">Header</AppBar>;
+    return (
+      <AppBar
+        position="fixed"
+        style={{
+          backgroundColor: "#2f2f2f",
+          boxShadow: "none",
+          padding: "10px 0px"
+        }}
+      >
+        <Toolbar>
+          <div className="header_logo">
+            <div className="font_righteous header_logo_venue">The Venue</div>
+            <div className="header_logo_title">Musical Events</div>
+          </div>
+        </Toolbar>
+      </AppBar>
+    );
   }
 }
 
