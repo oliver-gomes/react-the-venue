064527e427004729db3ecc8c06750ee302594a0b
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-31 17:10:52 -0400

    added content to text

---
+++ src/components/highlights/Description.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Fade from "react-reveal/Fade";
+
+const Description = () => {
+  return (
+    <Fade>
+      <div className="center_wrapper">
+        <h2>
+          Highlights
+          <div className="highlight_description">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
+            repudiandae mollitia accusamus animi ullam debitis obcaecati. Neque
+            commodi minus aliquid?
+          </div>
+        </h2>
+      </div>
+    </Fade>
+  );
+};
+
+export default Description;
