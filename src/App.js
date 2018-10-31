ba7d65cf9b582a85f661bd9376d90d952ef67233
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-30 21:50:46 -0400

    background color

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 import "./resources/styles.css";
 import Header from "./components/header-footer/Header";
 //import Featured from "./components/featured/Featured.js";
+import SideDrawer from "./components/header-footer/SideDrawer";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div
+        className="App"
+        style={{ height: "1500px", background: "cornflowerblue" }}
+      >
         <Header />
       </div>
     );
