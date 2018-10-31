f4f52c57f6c1b4de227a6317f5d2acb719783367
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-30 21:02:30 -0400

    reset project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./resources/styles.css";
 import Header from "./components/header-footer/Header";
+//import Featured from "./components/featured/Featured.js";
+
 class App extends Component {
   render() {
     return (
--- src/components/header-footer/Header.js
+++ src/components/header-footer/Header.js
@@ -1,30 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import MenuIcon from "@material-ui/core/Menu";
 import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
-class Header extends Component {
-  render() {
-    return (
-      <AppBar
-        position="fixed"
-        style={{
-          backgroundColor: "#2f2f2f",
-          boxShadow: "none",
-          padding: "10px 0px"
-        }}
-      >
-        <Toolbar>
-          <div className="header_logo">
-            <div className="font_righteous header_logo_venue">The Venue</div>
-            <div className="header_logo_title">Musical Events</div>
-          </div>
-        </Toolbar>
-      </AppBar>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <AppBar
+      position="fixed"
+      style={{
+        backgroundColor: "#2f2f2f",
+        boxShadow: "none",
+        padding: "10px 10px"
+      }}
+    >
+      <Toolbar>
+        <div className="header_logo">
+          <div className="font_righteous header_logo_venue">The Venue</div>
+          <div className="header_logo_title">Musical Events</div>
+        </div>
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Header;
