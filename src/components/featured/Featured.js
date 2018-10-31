9c235b60ce968da7cce4e91730a63f1c2af96985
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-31 13:37:34 -0400

    timeuntil imported and implemented

--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Carrousel from "./Carrousel";
+import TimeUntil from "./TimeUntil";
 
 const Featured = () => {
   return (
@@ -8,6 +9,8 @@ const Featured = () => {
       <div className="artist_name">
         <div className="wrapper">Ariana Grande</div>
       </div>
+
+      <TimeUntil />
     </div>
   );
 };
